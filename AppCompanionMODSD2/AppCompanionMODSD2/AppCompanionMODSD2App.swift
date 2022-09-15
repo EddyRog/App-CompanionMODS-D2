49ee1dbce3924dc77079e0ddfebe1c4ab0c1df66
Eddy.R <pro.eddy.r@gmail.com>
2022-09-15 14:42:26 +0200

    [DEV] Init proj

---
+++ AppCompanionMODSD2/AppCompanionMODSD2/AppCompanionMODSD2App.swift
@@ -0,0 +1,17 @@
+//
+// AppCompanionMODSD2App.swift
+// AppCompanionMODSD2
+// Created in 2022
+// Swift 5.0
+
+
+import SwiftUI
+
+@main
+struct AppCompanionMODSD2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
